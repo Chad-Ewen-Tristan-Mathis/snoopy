37ae7d7c176cc9cb00191534f6297b08343a9c8b
Mathis Alarcon <67607934+sckratt@users.noreply.github.com>
2023-10-23 21:15:15 +0200

    setup

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/mathi/CLionProjects/snoopy
+# Install script for directory: C:/Users/mathi/OneDrive/Documents/snoopy
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/mathi/CLionProjects/snoopy/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mathi/OneDrive/Documents/snoopy/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
